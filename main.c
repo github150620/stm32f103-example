a090622eb72070dff8a28032109882c464e62247
Roc <597540267@qq.com>
2019-10-12 16:31:03 +0800

    Update main.c

--- main.c
+++ main.c
@@ -2,12 +2,18 @@
 #include "stm32f10x.h"
 #include "cmsis_os.h"
 
-//#include "GPIO_STM32F10x.h"
-//#include "USART_STM32F10x.h"
+extern int Init_Thread_LED(void);
+extern int Init_Thread_GPRS(void);
 
 int main(void) {
+  
   osKernelInitialize();
-  osKernelStart();  
+  
+  Init_Thread_LED();
+  Init_Thread_GPRS();
+  
+  osKernelStart();
+  
   while (1) {
     osDelay(1000);
   }
