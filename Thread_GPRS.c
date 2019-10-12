176ec13224007c35132d623c98ae296e4297fa8d
Roc <597540267@qq.com>
2019-10-12 16:59:48 +0800

    Update Thread_GPRS.c

--- Thread_GPRS.c
+++ Thread_GPRS.c
@@ -8,6 +8,9 @@ void Thread_GPRS(void const *argument);
 osThreadId tid_Thread_GPRS;
 osThreadDef(Thread_GPRS, osPriorityNormal, 1, 0);
 
+osMessageQId mqid_Thread_GPRS;
+osMessageQDef(Thread_GPRS, 32, uint32_t);
+
 int Init_Thread_GPRS(void) {
   tid_Thread_GPRS = osThreadCreate(osThread(Thread_GPRS), NULL);
   if (!tid_Thread_GPRS) {
@@ -27,18 +30,27 @@ static void USART1_Callback(uint32_t event) {
 }
 
 void Thread_GPRS(void const *argument) {
-	Driver_USART1.Initialize(USART1_Callback);
-	Driver_USART1.PowerControl(ARM_POWER_FULL);
-	ret = Driver_USART1.Control(ARM_USART_MODE_ASYNCHRONOUS |
-                              ARM_USART_DATA_BITS_8 |
-                              ARM_USART_PARITY_NONE |
-                              ARM_USART_STOP_BITS_1 |
-                              ARM_USART_FLOW_CONTROL_NONE, 
-                              115200);
-	Driver_USART1.Control(ARM_USART_CONTROL_TX, 1);
-	Driver_USART1.Control(ARM_USART_CONTROL_RX, 1);
+
+  osEvent event;
+
+  mqid_Thread_GPRS = osMessageCreate(osMessageQ(Thread_GPRS), tid_Thread_GPRS);
+  if (!mqid_Thread_GPRS) {
+    return;
+  }
+
+  Driver_USART1.Initialize(USART1_Callback);
+  Driver_USART1.PowerControl(ARM_POWER_FULL);
+  Driver_USART1.Control(ARM_USART_MODE_ASYNCHRONOUS |
+                        ARM_USART_DATA_BITS_8 |
+                        ARM_USART_PARITY_NONE |
+                        ARM_USART_STOP_BITS_1 |
+                        ARM_USART_FLOW_CONTROL_NONE, 
+                        115200);
+  Driver_USART1.Control(ARM_USART_CONTROL_TX, 1);
+  Driver_USART1.Control(ARM_USART_CONTROL_RX, 1);
 
   while (1) {
-  
+    event = osMessageGet(mqid_Thread_GPRS, osWaitForever);
+    
   }
 }
