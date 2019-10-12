487e1cd5b1547fca93320a45680428603de806cd
Roc <597540267@qq.com>
2019-10-12 15:59:16 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+
+#include "stm32f10x.h"
+#include "cmsis_os.h"
+
+//#include "GPIO_STM32F10x.h"
+//#include "USART_STM32F10x.h"
+
+int main(void) {
+  osKernelInitialize();
+	osKernelStart();  
+  while (1) {
+    osDelay(1000);
+  }
+}
+
+
